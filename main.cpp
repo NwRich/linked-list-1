02ef52378e2bd96e4e5d2091ab833f12e15e13b6
U-BSD\278797 <278797@SHS-10DD3AC22.beaverton.k12.or.us>
2017-12-05 12:08:39 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "node.h"
+#include "student.h"
+
+using namespace std;
+void add(student* newStudent);
+void print(node* next);
+node* head;
+int main() {
+  student* n = new student(123);
+  add(n);
+  n = new student(321);
+  add(n);
+  print(head);
+  return 0;
+}
+
+void add(student* newStudent) {
+  node* current = head;
+  if (current == NULL) {
+    head = new node();
+    head->setValue(newStudent);
+  }
+  else {
+    while (current->getNext() != NULL) {
+      current = current->getNext();
+    }
+    current->setNext(new node());
+    current->getNext()->setValue(newStudent);
+  }
+}
+
+void print (node* next) {
+  if (next == head) {
+    cout << "list ";
+  }
+  if (next != NULL) {
+    cout << next->getValue()->getId() << " ";
+    print(next->getNext());
+  }
+}
+# linked-list-1
